ca200651da6b3f66872a4a859e5568823a0fd01b
mrdth <mrdth@zeventien.org>
2021-10-31 12:11:11 +0000

    Setup storybook for better typescript use

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +8,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss"
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  }
 }
